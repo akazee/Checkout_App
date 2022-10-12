940f1e9ab37ebd07682a54636b9f0312835710a0
akazee <56356510+akazee@users.noreply.github.com>
2022-10-12 17:43:48 -0400

    added frontend config

--- Checkout_App/Checkout_App/settings.py
+++ Checkout_App/Checkout_App/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'checkout.apps.CheckoutConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Checkout_App/checkout/views.py
+++ Checkout_App/checkout/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Checkout</h1>')
+    return render(request, 'checkout/checkout_home.html')
 
-# Create your views here.
+
