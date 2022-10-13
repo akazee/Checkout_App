3d01cab2a3225b654a5a36130c8eddee22e0ec8c
akazee <56356510+akazee@users.noreply.github.com>
2022-10-13 14:21:42 -0400

    added model classes, created base layout for display

--- Checkout_App/checkout/views.py
+++ Checkout_App/checkout/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import recipt
 
+# view config for base html
+def index(response, id):
+    ls = recipt.objects.get(id=id)
+    return render(response, "checkout/display.html", {"ls":ls, "subtotal":ls.subtotal, "disctotal":ls.discount_total})
+    
+# view config for home 
 def home(request):
-    return render(request, 'checkout/checkout_home.html')
-
+   return render(request, 'checkout/checkout_home.html', {})
 
